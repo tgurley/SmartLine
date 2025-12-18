c5857fc62a3643b3066a4055028345b6bf069d10
tgurley1 <157721216+tgurley1@users.noreply.github.com>
2025-12-17 19:55:09 -0600

    updated databse for railwa

--- smartline-api/app/database.py
+++ smartline-api/app/database.py
@@ -1,16 +1,14 @@
 import os
 import psycopg2
 from psycopg2.extras import RealDictCursor
-from dotenv import load_dotenv
-
-load_dotenv()
 
 def get_connection():
     return psycopg2.connect(
-        host=os.getenv("DB_HOST"),
-        dbname=os.getenv("DB_NAME"),
-        user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASSWORD"),
-        port=os.getenv("DB_PORT", 5432),
+        host=os.getenv("PGHOST"),
+        dbname=os.getenv("PGDATABASE"),
+        user=os.getenv("PGUSER"),
+        password=os.getenv("PGPASSWORD"),
+        port=os.getenv("PGPORT"),
         cursor_factory=RealDictCursor
     )
+
